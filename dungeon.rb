ca94522a7462a669744892ea1460b29adfbe1a12
Jeremy Tarling <jeremy.tarling@gmail.com>
2012-03-23 22:50:27 +0000

    replaced classes with structs

--- dungeon.rb
+++ dungeon.rb
@@ -1,26 +1,30 @@
 class Dungeon
   attr_accessor :player
+
   def initialize(player_name)
     @player = Player.new(player_name)
     @rooms = []
   end
 
-  class Player
-    attr_accessor :name, :location
-    def initialize(player_name)
-      @name - player_name
-    end
-  end
+  Player = Struct.new(:name, :location)
 
-  class Room
-    attr_accessor :reference, :name, :description, :connections
-    def initialize(reference, name, description, connections)
-      @reference = reference
-      @name = name
-      @description = description
-      @connections = connections
-    end
-  end
+#  class Player
+#    attr_accessor :name, :location
+#    def initialize(player_name)
+#      @name - player_name
+#    end
+#  end
+
+  Room = Struct.new(:reference, :name, :description, :connections)
+
+#  class Room
+#    attr_accessor :reference, :name, :description, :connections
+#    def initialize(reference, name, description, connections)
+#      @reference = reference
+#      @name = name
+#      @description = description
+#      @connections = connections
+#    end
+#  end
+
 end
-
-
