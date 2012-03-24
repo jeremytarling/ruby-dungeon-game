51ee756c7ba0b520f9888fd17ed3fb4f83a46296
Jeremy Tarling <jeremy.tarling@gmail.com>
2012-03-24 09:09:29 +0000

    added methods for moving around

--- dungeon.rb
+++ dungeon.rb
@@ -1,4 +1,5 @@
 class Dungeon
+
   attr_accessor :player
 
   def initialize(player_name)
@@ -6,25 +7,65 @@ class Dungeon
     @rooms = []
   end
 
-  Player = Struct.new(:name, :location)
+  def add_room(reference, name, description, connections)
+    @rooms << Room.new(reference, name, description, connections)
+  end
 
-#  class Player
-#    attr_accessor :name, :location
-#    def initialize(player_name)
-#      @name - player_name
-#    end
-#  end
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
 
-  Room = Struct.new(:reference, :name, :description, :connections)
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
 
-#  class Room
-#    attr_accessor :reference, :name, :description, :connections
-#    def initialize(reference, name, description, connections)
-#      @reference = reference
-#      @name = name
-#      @description = description
-#      @connections = connections
-#    end
-#  end
+  def find_room_in_dungeon(reference)
+    @rooms.detect { |room| room.reference == reference }
+  end
+
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_dungeon(direction)
+    show_current_description
+  end
+
+
+  class Player
+    
+    attr_accessor :name, :location
+    
+    def initialize(name)
+      @name = name
+    end
+  
+  end
+
+  class Room
+    
+    attr_accessor :reference, :name, :description, :connections
+    
+    def initialize(reference, name, description, connections)
+      @reference = reference
+      @name = name
+      @description = description
+      @connections = connections
+    end
+
+    def full_description
+      @name + "\n\nYou are in " + @description
+    end
+
+  end
 
 end
+
+# create the main dungeon object
+my_dungeon = Dungeon.new("Jeremy Tarling")
+
+# add some rooms
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", { :west => :smallcave })
+my_dungeon.add_room(:smallcave, "Small Cave", "a small claustrophobic cave", { :east => :largecave })
+
+# start in a large cave
+my_dungeon.start(:largecave)
